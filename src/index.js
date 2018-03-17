385645e170cdbd9a01322b3fe91a4615498f66e2
Kim Torberntsson <kimtorberntsson@gmail.com>
2018-03-17 17:05:35 +0100

    Prevent points from becoming smaller than zero

--- src/index.js
+++ src/index.js
@@ -85,28 +85,38 @@ class Game extends React.Component {
 	renderPlayerInformation(index) {
 		var width = {width: `${100/this.state.playerInformation.length}%`};
 		return (
-				<PlayerInformation
-					armyName={this.state.playerInformation[index].playerName}
-					playerName={this.state.playerInformation[index].armyName}
-					symbol={this.state.playerInformation[index].symbol}
-					commandPoints={this.state.playerInformation[index].commandPoints}
-					victoryPoints={this.state.playerInformation[index].victoryPoints}
-					key={this.state.playerInformation[index].playerName}
-					commandPointUpButtonClicked={() => this.onPointsButtonPressed(index, 1, 1)}
-					commandPointDownButtonClicked={() => this.onPointsButtonPressed(index, 1, 0)}
-					victoryPointUpButtonClicked={() => this.onPointsButtonPressed(index, 0, 1)}
-					victoryPointDownButtonClicked={() => this.onPointsButtonPressed(index, 0, 0)}
-					width={width}
-				/>
+			<PlayerInformation
+				armyName={this.state.playerInformation[index].playerName}
+				playerName={this.state.playerInformation[index].armyName}
+				symbol={this.state.playerInformation[index].symbol}
+				commandPoints={this.state.playerInformation[index].commandPoints}
+				victoryPoints={this.state.playerInformation[index].victoryPoints}
+				key={this.state.playerInformation[index].playerName}
+				commandPointUpButtonClicked={() => this.onPointsButtonPressed(index, 1, 1)}
+				commandPointDownButtonClicked={() => this.onPointsButtonPressed(index, 1, 0)}
+				victoryPointUpButtonClicked={() => this.onPointsButtonPressed(index, 0, 1)}
+				victoryPointDownButtonClicked={() => this.onPointsButtonPressed(index, 0, 0)}
+				width={width}
+			/>
 		);
 	}
 
 	onPointsButtonPressed(index, commandPoint, increase) {
 		var playerInformation = this.state.playerInformation;
 		if(commandPoint) {
-			increase ? playerInformation[index].commandPoints += 1 : playerInformation[index].commandPoints -= 1;
+			if (increase) {
+				playerInformation[index].commandPoints += 1
+			} else if (playerInformation[index].commandPoints > 0) {
+				// Never decrease beyond zero points
+				playerInformation[index].commandPoints -= 1;
+			}
 		} else {
-			increase ? playerInformation[index].victoryPoints += 1 : playerInformation[index].victoryPoints -= 1;
+			if (increase) {
+				playerInformation[index].victoryPoints += 1
+			} else if (playerInformation[index].victoryPoints > 0) {
+				// Never decrease beyond zero points
+				playerInformation[index].victoryPoints -= 1;
+			}
 		}
 		this.setState({
 			playerInformation: playerInformation
